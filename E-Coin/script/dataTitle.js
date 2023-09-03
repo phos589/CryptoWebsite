39ee2b44d563b5b53c9f4d42a70bf9ebd4750f85
Stefan C <140889919+phos589@users.noreply.github.com>
2023-09-03 12:04:29 +0200

    update html

---
+++ E-Coin/script/dataTitle.js
@@ -0,0 +1,47 @@
+export const featureData = [
+{
+  title: 'Secure & Reliable',
+  imageSrc: 'img/home/Secure & Reliable.png',
+  description: 'Our platform prioritizes the security of your assets with state-of-the-art encryption and authentication measures.'
+},
+{
+  title: 'User-Friendly',
+  imageSrc: 'img/home/User-Friendly.png',
+  description: 'We provide an intuitive interface for both beginners and experienced traders, making crypto trading a breeze'
+},
+{
+  title: 'Wide Range of Cryptos',
+  imageSrc: 'img/home/Wide Range of Cryptos.png',
+  description: 'Access a diverse portfolio of cryptocurrencies, ensuring you can trade the assets you love.'
+},
+{
+  title: 'Transparent Fees',
+  imageSrc: 'img/home/Transparent Fees.png',
+  description: 'We offer competitive and transparent fee structures, so you always know what you`re paying.'
+}
+,
+{
+  title: 'Confirm Transactions',
+  imageSrc: 'img/home/Confirm Transactions.png',
+  description: 'Easily confirm and verify your transactions for added security.'
+}
+,
+{
+  title: 'Connect Your Wallet',
+  imageSrc: 'img/home/Connect Your Wallet.png',
+  description: 'Seamlessly connect your cryptocurrency wallet to our platform for convenient trading and management.'
+}
+,
+{
+  title: 'Collections',
+  imageSrc: 'img/home/Collections.png',
+  description: 'Organize your assets and investments into custom collections for better portfolio management.'
+}
+,
+{
+  title: 'Expert Support',
+  imageSrc: 'img/home/Expert Support.png',
+  description: 'Our dedicated support team is available 24/7 to assist you with any questions or issues.'
+}
+
+];
--- E-Coin/script/home.js
+++ E-Coin/script/home.js
@@ -1,4 +1,5 @@
 import { cryptoData } from '../script/data.js';
+import { featureData } from '../script/dataTitle.js';
 
 const coinList = document.getElementById('coinList');
 const pagination = document.getElementById('pagination');
@@ -72,3 +73,28 @@ function updatePaginationButtons() {
 displayCoins(0, coinsPerPage);
 createPaginationButtons();
 updatePaginationButtons();
+
+const featureGrid = document.querySelector('.feature-grid');
+
+// Loop through featureData and create HTML for each item
+featureData.forEach(feature => {
+  // Create a div for each feature
+  const featureDiv = document.createElement('div');
+  featureDiv.classList.add('feature');
+
+  // Create an h3 element with the title and the image
+  const h3Element = document.createElement('h3');
+  h3Element.innerHTML = `${feature.title} <img src="${feature.imageSrc}" style="width: 70px; margin-left: 10px;">`;
+
+  // Create a p element with the description
+  const pElement = document.createElement('p');
+  pElement.textContent = feature.description;
+
+  // Append h3 and p elements to the featureDiv
+  featureDiv.appendChild(h3Element);
+  featureDiv.appendChild(pElement);
+
+  // Append the featureDiv to the featureGrid
+  featureGrid.appendChild(featureDiv);
+});
+
